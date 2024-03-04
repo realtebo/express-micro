c3333e98205d2bd6bc930c5b6996794cbfd17b48
Mirko Tebaldi <mirkotebaldi@yahoo.it>
2024-03-04 18:22:06 +0100

    wip react

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {routeHello, routeAPINames, routeWeather} from "./routes.js";
 import express, {Request, Response} from "express";
+import path from "path";
 
 const server = express();
 const port = 3000;
@@ -26,6 +27,12 @@ server.get(
         res.send(response);
     }
 );
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
 
 server.listen(port, function () {
     console.log('Listening on ' + port);
